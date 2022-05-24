980ba742660cc409045c002e37c1009212638cd5
niemoBroIntegration <49417819+niemoBroIntegration@users.noreply.github.com>
2022-05-24 00:46:44 -0400

    blue top buttons

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,31 +11,40 @@ import Add from "./views/Add";
 import About from "./views/About";
 
 function App() {
-  return (
-    <div className="App">
-      <h4 className="align-middle">
-        <a className="btn btn-light btn-sm px-3 mx-2" href="http://44.201.88.86/about">About Pirate Crew</a>
-        <a className="btn btn-light btn-sm px-3 mx-2" href="http://52.70.161.67/projects">Eric Niemeyer Projects</a>
-      </h4>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/add">
-          <Add />
-        </Route>
-        <Route exact path="/about">
-          <About />
-        </Route>
-        <Route exact path="/:_id">
-          <Single />
-        </Route>
-        <Route exact path="/:_id/edit">
-          <Edit />
-        </Route>
-      </Switch>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="d-flex">
+            
+                    <a className="bbb" href="http://44.201.88.86/about">
+                        <p className="bbbt">About Pirates</p>
+                    </a>
+
+                    <a className="bbb" href="http://52.70.161.67/projects">
+                        <p className="bbbt">Projects Home</p>
+                    </a>
+         
+                {/* <a className="btn btn-light btn-sm px-3 mx-2" href="http://44.201.88.86/about">About Pirate Crew</a>
+        <a className="btn btn-light btn-sm px-3 mx-2" href="http://52.70.161.67/projects">Eric Niemeyer Projects</a> */}
+            </div>
+            <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route exact path="/add">
+                    <Add />
+                </Route>
+                <Route exact path="/about">
+                    <About />
+                </Route>
+                <Route exact path="/:_id">
+                    <Single />
+                </Route>
+                <Route exact path="/:_id/edit">
+                    <Edit />
+                </Route>
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
